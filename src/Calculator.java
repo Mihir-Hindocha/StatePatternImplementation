3781b80095c05dff7488887f55673783b6ebde6d
Mihir Hindocha <mihirhindocha97@gmail.com>
2018-10-15 18:47:14 -0500

    Initial Project Setup

---
+++ src/Calculator.java
@@ -0,0 +1,7 @@
+/**
+ * This class will implement the Arithmetic operations of + and - on the input positive expressions.
+ * */
+
+public class Calculator {
+
+}
---
+++ src/State.java
@@ -0,0 +1,7 @@
+/**
+ * This interface will be implemented by the various different states.
+ * */
+
+public interface State {
+
+}
